92af9306109132a3f492984984d741f655ce41e1
Mickael Daniel <daniel.mickael@gmail.com>
2012-04-22 23:14:36 +0200

    cssline start, input multi-line CSS, output one-line format

---
+++ cssline/cssline.js
@@ -0,0 +1,142 @@
+#!/usr/bin/env node
+
+//
+// small program/library to make creation of data uris for web content quick and easy
+//
+//    ./base64 ../path/to/some/file.png | pbcopy
+//
+
+var fs = require('fs'),
+  path = require('path'),
+  util = require('util'),
+  nopt = require('nopt'),
+  request = require('request'),
+  Stream = require('stream').Stream,
+  Recess = require('recess').Constructor;
+
+module.exports = cssline;
+cssline.CSSLine = CSSLine;
+
+function cssline(file, options) {
+  var url = /\/\//.test(file);
+  var input = url ? request(file) : fs.createReadStream(file);
+  return input.pipe(new CSSLine(options)).pipe(process.stdout);
+}
+
+function CSSLine(options) {
+  this.readable = this.writable = true;
+  this.chunks = [];
+  this.options = options || {};
+
+  // unfortunately, recess only allows filepath to be passed-in not raw content
+  // so write the file temporary in a local cache, then create a new Recess
+
+  var self = this;
+  this.on('pipe', function(input) {
+    self.cachefile = path.join(__dirname, '_cache', (+new Date + '').slice(-10) + '.css');
+    input.pipe(fs.createWriteStream(self.cachefile));
+  });
+
+  this.on('clean', function() {
+    fs.unlink(self.cachefile)
+  });
+
+  this.on('recess', function(e) {
+    var r = self.recess;
+    var out = r.definitions.map(self.toCSS).join('\n');
+
+    self.emit('data', out + '\n');
+    self.emit('clean');
+  });
+
+  Stream.call(this);
+}
+
+util.inherits(CSSLine, Stream);
+
+// take a less token, returns according css
+CSSLine.prototype.toCSS = function(t) {
+  if(t.value) return '\n' + t.value.trim();
+
+  var sel = t.selectors.map(function(sel) {
+    return sel.elements.map(function(el) {
+      return el.combinator.value + el.value;
+    }).join('');
+  }).join(',');
+
+  var rules = t.rules.map(function(r) {
+    var value = r.value.value;
+    if(!value) return '';
+    return r.name + ': ' + value.map(function(v) {
+      var val = Array.isArray(v.value) ? v.value : [{
+        value: v.value
+      }];
+
+      // should probably be better if put as a recursive thing
+      function value(parent) { return function(v) {
+        var prev = parent.value && parent.value.value;
+        // value is an inner prop, build from parent value
+        if(typeof v.value === 'object') {
+          // is it an url like thing?
+          if(v.paths) return 'url(' + v.value.value + ')';
+        }
+
+        // is a direct value and not an inner prop
+        if(v.value != null) return v.value + (v.unit || '');
+
+        // name is set, iterate over args and build appropriate output
+        if(!v.name) return;
+        var args = v.args.map(function(a) {
+          return a.value.map(value(v.name));
+        }).join(', ');
+        return v.name + '(' + args + ')';
+      }}
+
+      return val.map(value(r)).join(' ').trim();
+    }).join('');
+  }).join('; ');
+
+  return sel.trim() + ' { ' + rules + ' }';
+};
+
+// stream api
+
+CSSLine.prototype.write = function(c) {
+  this.chunks = this.chunks.concat(c);
+};
+
+CSSLine.prototype.end = function() {
+  var body = this.chunks.join('');
+  this.recess = new Recess(this.cachefile, {
+    compile: true,
+    oneline: true
+  }, this.emit.bind(this, 'recess'));
+};
+
+// cssline api
+
+
+// run!
+var opts = nopt({
+  help: Boolean,
+  version: Boolean,
+  oneline: Boolean,
+  multiline: Boolean
+}, {
+  h: '--help',
+  v: '--version',
+  o: '--oneline',
+  m: '--multiline',
+  single: '--oneline',
+  multi: '--multiline'
+});
+
+var file = opts.argv.remain[0];
+if(file) return cssline(file, opts);
+
+if(opts.help) return fs.createReadStream(path.join(__dirname, 'readme.md')).pipe(process.stdout);
+if(opts.version) return console.log(require('./package.json').version);
+
+// read from stdin at this point
+process.openStdin().pipe(new CSSLine(opts)).pipe(process.stdout);
+
---
+++ cssline/readme.md
@@ -0,0 +1,25 @@
+
+*small program/library to convert style sheets from multi-line CSS to single-line, and vice-versa*
+
+This tool internally uses [recess][] to parse CSS files, and work on top
+of `recess.definitions` as parsed by the LESS tokenizer.
+
+---
+
+## Synopsis
+
+You can "single-line" a "multi-line" css
+
+    $ cat ./path/to/some/file.css --oneline
+
+And you can "multi-line" a "single-line" css
+
+    $ css ./path/to/some/file.css --multiline
+
+You can also convert directly a remote file, it just needs to have `//` in
+it to be considered as a remote url.
+
+    $ css https://raw.github.com/necolas/suit/master/css/suit.css --oneline
+
+[recess]: http://twitter.github.com/recess
+
