de533186b660db0e74a0b31ffd0265a654645a29
Mickael Daniel <daniel.mickael@gmail.com>
2012-08-08 16:27:44 +0200

    templates autoload & precompile

---
+++ tools/impress2md/templates/index.js
@@ -0,0 +1,14 @@
+
+var fs = require('fs'),
+  path = require('path'),
+  _ = require('underscore');
+
+// Build up a hash of precompiled (underscore) templates, automatically
+// from the list of html files in this given directory.
+var templates = module.exports;
+fs.readdirSync(__dirname).forEach(function(file) {
+  if(file === 'index.js') return;
+
+  var content = fs.readFileSync(path.join(__dirname, file), 'utf8');
+  templates[file] = _.template(content);
+});
