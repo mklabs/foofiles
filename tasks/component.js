06c211e8d58fbdeaa72812f046f6bcd756d1cfab
Mickael Daniel <daniel.mickael@gmail.com>
2012-09-26 01:29:41 +0200

    add build

--- tasks/component.js
+++ tasks/component.js
@@ -2,7 +2,6 @@
 var fs        = require('fs');
 var path      = require('path');
 var spawn     = require('child_process').spawn;
-var component = require('component');
 
 // TODO: Implement specific tasks for component commands not catched up by yeoman as part
 // of a bower command: convert, create, open, ...
@@ -21,16 +20,7 @@ module.exports = function(grunt) {
     var args = this.args.slice(1);
 
     // cli flags
-    var opts = Object.keys(grunt.cli.options).filter(function(key) {
-      return !/tasks|npm/.test(key);
-    });
-
-    if(opts.length) opts = opts.map(function(key) {
-      var val = grunt.cli.options[key];
-      if(val === false) return '';
-      if(val === true) return '--' + key;
-      return '--' + key + ' ' + val;
-    }).join(' ').split(' ');
+    var opts = grunt.helper('component:opts', grunt.cli.options);
 
     // alias list to ls
     cmd = cmd === 'list' ? 'ls' : cmd;
@@ -47,7 +37,8 @@ module.exports = function(grunt) {
     var cb = this.async();
     // stdio: inherit in 0.8
     args = args.concat(opts);
-    console.log('spawning', args, opts);
+    grunt.log.write('... ' + path.basename(bin) + ' ' + args.join(' ') + '...');
+    console.log('-->', args);
     var proc = spawn(bin, args);
     proc.stdout.pipe(process.stdout);
     proc.stderr.pipe(process.stderr);
@@ -55,4 +46,52 @@ module.exports = function(grunt) {
       cb(code === 0);
     });
   });
+
+
+  // build interface
+  grunt.registerTask('component', 'Build interface to component', function() {
+    var config = grunt.config('component');
+
+    var component = grunt.file.readJSON('component.json');
+
+    // do the globbing and resolve scripts / styles / templates
+    ['scripts', 'styles', 'templates'].forEach(function(key) {
+      var files = config[key];
+      component[key] = files ? grunt.file.expandFiles(files) : [];
+    });
+
+    var body = JSON.stringify(component, null, 2);
+    grunt.verbose.writeln().write('Writing component.json with:\n' + body).writeln();
+    grunt.file.write('component.json', body);
+    grunt.log.write('Updating component.json...').ok();
+
+
+    var builder = path.join(basedir, 'bin/component-build');
+
+    var opts = grunt.config('component.options') || {};
+    var args = grunt.helper('component:opts', opts);
+
+    var cb = this.async();
+    grunt.log.write('... component-build ' + args.join(' ') + '...');
+    var proc = spawn(builder, args);
+    proc.stdout.pipe(process.stdout);
+    proc.stderr.pipe(process.stderr);
+    proc.on('exit', function(code) {
+      grunt.log.ok();
+      cb(code === 0);
+    });
+
+  });
+
+  grunt.registerHelper('component:opts', function(opts) {
+    return Object.keys(opts || {}).filter(function(key) {
+      return !(/tasks|npm/).test(key);
+    }).map(function(key) {
+      var val = opts[key];
+      if(val === false) return '';
+      if(val === true) return '--' + key;
+      return '--' + key + ' ' + val;
+    }).join(' ').split(' ');
+  });
+
 };
